9d5aada6481077eefbcc54e4c993428274528d23
imyj1013 <imyj1013@gmail.com>
2025-03-26 19:25:38 +0900

    Add project files

---
+++ backend/src/main/java/com/veriview/backend/entity/TestEntity.java
@@ -0,0 +1,15 @@
+package com.veriview.backend.entity;
+
+import javax.consistance.Column;
+import javax.consistance.Entity;
+import javax.consistance.Id;
+
+@Entity
+public class TestEntity {
+
+    @Id
+    @Column(nullable = false)
+
+
+
+}
