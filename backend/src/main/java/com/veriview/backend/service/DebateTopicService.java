c6bd946b482a1efc0ac9d1645251df52dd9780be
imyj1013 <imyj1013@gmail.com>
2025-04-08 07:21:02 +0900

    Refactor Simplify job posting crawling logic

--- backend/src/main/java/com/veriview/backend/service/DebateTopicService.java
+++ backend/src/main/java/com/veriview/backend/service/DebateTopicService.java
@@ -19,24 +19,21 @@ public class DebateTopicService {
     @Autowired
     private DebateTopicRepository debateTopicRepository;
 
-    private static final String BASE_URL = "https://joberum.com/interview/zocbo/zocbo01.asp?SEARCH_FIELD=TITLE&SEARCH_VALUE=&ORDER_FIELD=WR_DATE&ORDER_METHOD=DESC&pc=1&INTERVIEW_GROUP=INTERVIEW03&INTERVIEW_DETAIL=&m_tab_1=&m_tab_2=3&m_tab_3=&DET_GROUP_TXT=&DET_GROUP_POP_STATE=False&INTERVIEW_SELECTED=&pgsize=100&page=";
+    private static final String BASE_URL = "https://joberum.com/interview/zocbo/zocbo01.asp?SEARCH_FIELD=TITLE&SEARCH_VALUE=&ORDER_FIELD=WR_DATE&ORDER_METHOD=DESC&pc=1&INTERVIEW_GROUP=INTERVIEW03&INTERVIEW_DETAIL=&m_tab_1=&m_tab_2=3&m_tab_3=&DET_GROUP_TXT=&DET_GROUP_POP_STATE=False&INTERVIEW_SELECTED=&pgsize=1035&page=1";
 
     @Transactional
     public void crawlAndSaveDebateTopics() {
         try {
-            for (int page = 1; page <= 11; page++) {
-                String url = BASE_URL + page;
-                Document doc = Jsoup.connect(url).get();
-                Elements topics = doc.select("a.cbt"); // `class="cbt"`인 링크 태그 찾기
+            Document doc = Jsoup.connect(BASE_URL).get();
+            Elements topics = doc.select("a.cbt"); // `class="cbt"`인 링크 태그 찾기
 
-                for (Element topic : topics) {
-                    String topicText = topic.text().trim();
-                    
-                    // 중복 저장 방지
-                    if (debateTopicRepository.findByTopic(topicText).isEmpty()) {
-                        Long nextId = debateTopicRepository.count() + 1; // ID 값을 직접 설정
-                        debateTopicRepository.save(new DebateTopic(nextId, topicText));
-                    }
+            for (Element topic : topics) {
+                String topicText = topic.text().trim();
+                
+                // 중복 저장 방지
+                if (debateTopicRepository.findByTopic(topicText).isEmpty()) {
+                    Long nextId = debateTopicRepository.count() + 1; // ID 값을 직접 설정
+                    debateTopicRepository.save(new DebateTopic(nextId, topicText));
                 }
             }
         } catch (IOException e) {
