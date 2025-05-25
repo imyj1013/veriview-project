a69dbbe537976bb6bc9fce61d9e29f1febf79952
imyj1013 <imyj1013@gmail.com>
2025-05-25 23:20:50 +0900

    feat: implement job recommendation

---
+++ backend/src/main/java/com/veriview/backend/controller/RecruitmentController.java
@@ -0,0 +1,25 @@
+package com.veriview.backend.controller;
+
+import com.veriview.backend.model.*;
+import com.veriview.backend.service.AuthService;
+import com.veriview.backend.service.RecruitmentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/recruitment")
+@RequiredArgsConstructor
+public class RecruitmentController {
+
+    private final RecruitmentService recruitmentService;
+
+    @PostMapping("/start")
+    public ResponseEntity<RecruitmentResponse> recruitment(@RequestBody RecruitmentRequest request) {
+
+        RecruitmentResponse res = recruitmentService.recommendation(request);
+        return ResponseEntity.ok(res);
+    }
+}
